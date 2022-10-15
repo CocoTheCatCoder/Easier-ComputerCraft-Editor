34ad07355af9b022dc039f6c58b5f774f05a623c
CocoTheCatVloger <CocoTheCatVloger0@gmail.com>
2022-10-15 10:04:34 -0500

    Created Relatively Safe way to hold Pastebin info

---
+++ index.js
@@ -0,0 +1,5 @@
+// Get Environment Variables
+import dotenv from 'dotenv';
+dotenv.config();
+const Keys = [process.env.DEVKEY, process.env.USRNAME, process.env.PASSWRD];
+// Delete Old Paste and Replace it with new Paste
